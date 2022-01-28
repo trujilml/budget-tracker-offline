4fa420de4786b0a9520a8e86cb5724c2a6b22acc
trujilml <mickeytrj@gmail.com>
2022-01-28 14:24:10 -0600

    worked and completed on service-worker js file - test out and continue service worker file alongside testing

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const APP_PREFIX = 'BudgetTrack-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "/",
+    "./index.html",
+    "./css/styles.css",
+    "./icon-72x72.png",
+    "./icon-96x96.png",
+    "./icon-128x128.png",
+    "./icon-144x144.png",
+    "./icon-152x152.png",
+    "./icon-192x192.png",
+    "./icon-384x384.png",
+    "./icon-512x512.png",
+    "./js/idb.js",
+    "./js/index.js",
+    "./manifest.json"
+];
