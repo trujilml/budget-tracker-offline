40fef8110204449826976ece7e97daaf8ac69534
trujilml <mickeytrj@gmail.com>
2022-02-02 19:44:43 -0600

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,6 @@ const APP_PREFIX = 'BudgetTrack-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    '/',
     './index.html',
     './css/styles.css',
     './js/index.js',
