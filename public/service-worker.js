a92a8046b6da3c4843c2b6cda2391669dc482169
trujilml <mickeytrj@gmail.com>
2022-02-05 11:04:38 -0600

    updated fetch request of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTrack-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "DATA_CACHE_" + VERSION;
 const FILES_TO_CACHE = [
     '/',
     './index.html',
@@ -47,17 +48,52 @@ self.addEventListener('activate', function (e) {
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log("Fetching the request..." + e.request.url);
-    e.respondWith( 
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log("Responding with cache: " + e.request.url);
-                return request;
-            } else {
-                console.log("The file is not cached, fetching : " + e.request.url);
-                return fetch(e.request);
-            }
-        })
+// self.addEventListener('fetch', function (e) {
+//     console.log("Fetching the request..." + e.request.url);
+//     e.respondWith( 
+//         caches.match(e.request).then(function (request) {
+//             if (request) {
+//                 console.log("Responding with cache: " + e.request.url);
+//                 return request;
+//             } else {
+//                 console.log("The file is not cached, fetching : " + e.request.url);
+//                 return fetch(e.request);
+//             }
+//         })
+//     );
+// });
+
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
     );
-});
+  });
